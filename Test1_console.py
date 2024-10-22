63c21392a1eb6e87c6df464d4e7a48cf0fa923c6
Paola Vilaseca <pao_anahi_8@hotmail.com>
2024-10-22 19:57:31 -0400

    fixed bugs when going back in options 2 and 4

--- Test1_console.py
+++ Test1_console.py
@@ -22,6 +22,7 @@ def check_server_status():
 
 # Funcion para mostrara el menu principal
 def start_program(inputQueue):
+    print("")
     print("========================================")
     print("=============BIENVENIDO=================")
     print("========================================")
@@ -71,27 +72,28 @@ def main():
                     print("Error: No se pudo conectar al servidor Flask.")
 
         elif option == "2":
-            print("========================================")
-            print("===========Eliminar tarea===============")
-            print("===(Escriba 'atras' para volver atras)===")
-            print("========================================")
-            print("")
-            task_id  = input("Ingrese el id de la tarea: ")
-            print("========================================")
+            while True:
+                print("========================================")
+                print("===========Eliminar tarea===============")
+                print("===(Escriba 'atras' para volver atras)===")
+                print("========================================")
+                print("")
+                task_id  = input("Ingrese el id de la tarea: ")
+                print("========================================")
 
-            if task_id == "atras":
-                continue
-            else:
-                try:
-                    response = requests.delete(f"{API_URL}/tareas/{task_id}")
-                    if response.status_code == 200:
-                        print(f"Tarea con ID {task_id} eliminada correctamente.")
-                    elif response.status_code == 404:
-                        print(f"No se encontró ninguna tarea con ID {task_id}.")
-                    else:
-                        print("Error al eliminar la tarea.")
-                except requests.exceptions.ConnectionError:
-                    print("Error: No se pudo conectar al servidor Flask.")
+                if task_id == "atras":
+                    break  
+                else:
+                    try:
+                        response = requests.delete(f"{API_URL}/tareas/{task_id}")
+                        if response.status_code == 200:
+                            print(f"Tarea con ID {task_id} eliminada correctamente.")
+                        elif response.status_code == 404:
+                            print(f"No se encontró ninguna tarea con ID {task_id}.")
+                        else:
+                            print("Error al eliminar la tarea.")
+                    except requests.exceptions.ConnectionError:
+                        print("Error: No se pudo conectar al servidor Flask.")
 
         elif option == "3":
             print("========================================")
@@ -105,34 +107,34 @@ def main():
             
 
         elif option == "4":
-            print("========================================")
-            print("===== Lista de tareas pendientes =======")
-            print("===(Escriba 'atras' para volver atras)===")
-            print("========================================")
-            print("")
-            
-            try:
-                response = requests.get(f"{API_URL}/tareas/pendientes")
-                if response.status_code == 200:
-                    pending_tasks = response.json()["tasks"]
-                    if pending_tasks:
-                        for task in pending_tasks:
-                            print(f"ID: {task['id']} - Tarea: {task['task']} - Estado: Pendiente")
+            while True:
+                print("========================================")
+                print("===== Lista de tareas pendientes =======")
+                print("===(Escriba 'atras' para volver atras)===")
+                print("========================================")
+                try:
+                    response = requests.get(f"{API_URL}/tareas/pendientes")
+                    if response.status_code == 200:
+                        pending_tasks = response.json()["tasks"]
+                        if pending_tasks:
+                            for task in pending_tasks:
+                                print(f"ID: {task['id']} - Tarea: {task['task']} - Estado: Pendiente")
+                        else:
+                            print("No hay tareas pendientes.")
                     else:
-                        print("No hay tareas pendientes.")
-                else:
-                    print("Error al obtener las tareas pendientes.")
-            except requests.exceptions.ConnectionError:
-                print("Error: No se pudo conectar al servidor.")
-                break
-            print("========================================")
-            keyboard = input()
+                        print("Error al obtener las tareas pendientes.")
+                except requests.exceptions.ConnectionError:
+                    print("Error: No se pudo conectar al servidor.")
+                    break
 
-            if keyboard == "atras":
-                continue
-            else:
-                print("Por favor, escriba 'atras' para salir.")
-            print("========================================")
+                print("========================================")
+                keyboard = input("Escriba 'atras' para volver: ")
+
+                if keyboard.lower() == "atras":
+                    break
+                else:
+                    print("Por favor, escriba 'atras' para salir.")
+                print("========================================")
 
 
         # El usuario debe escribir guardar para que las tareas se guarden
